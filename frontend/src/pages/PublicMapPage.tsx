61ef2b4a1835672ee18ca0bded62695cf5f957cb
Valery Kirkizh <valery@kirkizh.com>
2026-09-16 18:19:13 +0200

    Configure CARTO map tiles

--- frontend/src/pages/PublicMapPage.tsx
+++ frontend/src/pages/PublicMapPage.tsx
@@ -11,6 +11,8 @@ import {
 import type {PublicMapResponse} from "../types";
 import "./PublicMapPage.css";
 
+const cartoApiKey = import.meta.env.VITE_CARTO_API_KEY?.trim() ?? "";
+
 type Props = {
   apiBaseUrl: string;
   username: string;
@@ -69,8 +71,8 @@ export function PublicMapPage({apiBaseUrl, username}: Props) {
         />
 
         <TileLayer
-          attribution='<a href="https://www.openstreetmap.org/copyright" target="_blank" rel="nofollow">OpenStreetMap</a>'
-          url="https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png"
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright" target="_blank" rel="nofollow">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions" target="_blank" rel="nofollow">CARTO</a>'
+          url={`https://basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png?key=${encodeURIComponent(cartoApiKey)}`}
         />
 
         {data.places.map((place) => (
